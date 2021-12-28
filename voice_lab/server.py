0e9172fa6e3fb409d9949577dcdcd4d1fbc85990
EC2 Default User <ec2-user@ip-10-20-0-158.ap-northeast-1.compute.internal>
2021-12-28 06:30:21 +0000

    change port num

--- voice_lab/server.py
+++ voice_lab/server.py
@@ -49,8 +49,8 @@ if __name__ == "__main__":
     arg_parser = ArgumentParser(
         usage='Usage: python ' + __file__ + ' [--port ] [--help]'
     )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
+    arg_parser.add_argument('-p', '--port', default=5000, help='port')
     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
     options = arg_parser.parse_args()
 
-    app.run(debug=options.debug, port=options.port)
+    app.run(host='0.0.0.0', debug=options.debug, port=options.port)
