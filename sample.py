d2fdc7d5b47c8e8bdeb30984dd204ca130f1eba7
Azuma Hiroki <ornitho1027@gmail.com>
2022-01-02 19:12:22 +0900

    edit siren volume

--- sample.py
+++ sample.py
@@ -3,6 +3,8 @@ import time
 
 cmd = "mplayer siren.mp3"
 p = subprocess.Popen("exec " + cmd, shell=True)
-time.sleep(10)
+time.sleep(1)
+# p.kill()
+wav = subprocess.Popen("aplay open_jtalk.wav", shell=True)
+wav.wait()
 p.kill()
-subprocess.Popen("aplay open_jtalk.wav", shell=True)
